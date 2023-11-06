707e7f71d75ee25c03404d0654819830d706fd36
heisenberg0611 <jonathanluis.jlcb@gmail.com>
2023-11-06 13:36:37 +0800

    fixed

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,7 +16,7 @@ const Work = () => {
       id="work"
       className="w-full h-full text-gray-300 bg-[#0a192f]"
     >
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-[1000px]">
         <motion.div
           className=""
           initial="hidden"
