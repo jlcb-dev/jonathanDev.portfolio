5a12cb9dd792f73ab18ea759d1c024cb8e86cfdd
Jonathan Batac <jonathanluis.jlcb@gmail.com>
2023-11-06 11:24:41 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import {
   FaTimes,
   FaGithub,
   FaLinkedin,
-  FaFacebook,
 } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
