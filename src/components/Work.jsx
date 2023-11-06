2e32cd15649d0cea63b7f525be8dbbe91245b857
heisenberg0611 <jonathanluis.jlcb@gmail.com>
2023-11-06 16:43:23 +0800

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,7 +16,7 @@ const Work = () => {
       id="work"
       className="pt-10 pb-24 text-gray-300 bg-[#0a192f]"
     >
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-[1200px]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-[1100px]">
         <motion.div
           className=""
           initial="hidden"
